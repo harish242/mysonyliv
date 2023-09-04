b93f4e8d0d79bc1ab3417cc5583c92a757c91ae1
harish242 <markollh242@gmail.com>
2023-09-04 09:02:08 +0530

    dgdhfg

--- src/Components/component/Registration.js
+++ src/Components/component/Registration.js
@@ -1,88 +1,174 @@
 
-import { useDispatch,useSelector } from 'react-redux';
+// import { useDispatch,useSelector } from 'react-redux';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
+
+// export function Regis() {
+//     const store=useSelector(state=>state.others.regisReducer)
+//     const navigate=useNavigate()
+
+//     console.log('regis/7',store)
+//     const dispatch=useDispatch()
+//     const handleInput=(e)=>{
+//         const idn=e.target.id
+//         console.log(idn)
+//         switch(idn){
+//             case 'username':return dispatch({type:'usernamegis',payload:e.target.value})
+//             case 'email':return dispatch({type:'emailgis',payload:e.target.value})
+//             case 'password':return dispatch({type:'passwordgis',payload:e.target.value})
+//         }
+//     } 
+//     const doPost=(e)=>{
+//         e.preventDefault()
+//         try{
+//              (async()=>{
+//                 const response=await fetch('https://academics.newtonschool.co/api/v1/user/signup',
+//                 {method:"POST",headers:{ 'Content-Type': 'application/json','projectId': 'xybcw190kyb8'},
+//                 body:JSON.stringify({...store,"appType":"ott"})
+//             }
+//                 )
+//                 if (!response.ok) {
+//                   const errorMessage = `Fetch error: ${response.statusText}`;
+//                   console.error(errorMessage);
+//                   throw new Error(errorMessage);
+//                 }
+//                 const data=await response.json()
+
+//                 if(data.status==='success'){
+//                   navigate('/login')
+//                 }
+//                 console.log(data)
+//              })()
+//         }catch(err){
+//          console.log(err)
+//         }
+//     }
+//   return(
+//     <div style={{height:'100vh',width:'100vw',backgroundImage:'url(https://platinmods.com/attachments/sonyliv-jarvismods-png.240209/)',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
+//       <div style={{display:"flex",justifyContent:'center',alignItems:'center',position:'relative',top:'100px'}}>
+// <div class="w-full max-w-xs" style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)', padding: '20px', borderRadius: '10px' ,position:'relative',left:'400px'}}>
+//   <form  onChange={handleInput} onSubmit={doPost}>
+//     <div class="mb-4">
+//       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+//         Username
+//       </label>
+//       <input  class="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+//     </div>
+//     <div class="mb-4">
+//       <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+//         Email
+//       </label>
+//       <input class="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email"/>
+//     </div>
+//     <div class="mb-6">
+//       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+//         Password
+//       </label>
+//       <input class="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+//       {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+//     </div>
+//     <div class="flex items-center justify-between">
+//       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+//         Sign In
+//       </button>
+//       <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/login">
+//         Update Password?
+//       </a>
+//     </div>
+//   </form>
+//   <p class="text-center text-gray-5000 text-md">
+//     &copy;2020 Already have an account?<Link
+//      to='/login'>login</Link>
+//   </p>
+// </div>
+// </div>
+//     </div>
+
+//   )
+// }
+
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
 
 export function Regis() {
-    const store=useSelector(state=>state.others.regisReducer)
-    const navigate=useNavigate()
+    const store = useSelector(state => state.others.regisReducer);
+    const navigate = useNavigate();
 
-    console.log('regis/7',store)
-    const dispatch=useDispatch()
-    const handleInput=(e)=>{
-        const idn=e.target.id
-        console.log(idn)
-        switch(idn){
-            case 'username':return dispatch({type:'usernamegis',payload:e.target.value})
-            case 'email':return dispatch({type:'emailgis',payload:e.target.value})
-            case 'password':return dispatch({type:'passwordgis',payload:e.target.value})
+    const dispatch = useDispatch();
+
+    const handleInput = (e) => {
+        const idn = e.target.id;
+        switch (idn) {
+            case 'username':
+                return dispatch({ type: 'usernamegis', payload: e.target.value });
+            case 'email':
+                return dispatch({ type: 'emailgis', payload: e.target.value });
+            case 'password':
+                return dispatch({ type: 'passwordgis', payload: e.target.value });
         }
-    } 
-    const doPost=(e)=>{
-        e.preventDefault()
-        try{
-             (async()=>{
-                const response=await fetch('https://academics.newtonschool.co/api/v1/user/signup',
-                {method:"POST",headers:{ 'Content-Type': 'application/json','projectId': 'xybcw190kyb8'},
-                body:JSON.stringify({...store,"appType":"ott"})
+    };
+
+    const doPost = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('https://academics.newtonschool.co/api/v1/user/signup', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json', 'projectId': 'xybcw190kyb8' },
+                body: JSON.stringify({ ...store, "appType": "ott" })
+            });
+            if (!response.ok) {
+                const errorMessage = `Fetch error: ${response.statusText}`;
+                console.error(errorMessage);
+                throw new Error(errorMessage);
             }
-                )
-                if (!response.ok) {
-                  const errorMessage = `Fetch error: ${response.statusText}`;
-                  console.error(errorMessage);
-                  throw new Error(errorMessage);
-                }
-                const data=await response.json()
+            const data = await response.json();
 
-                if(data.status==='success'){
-                  navigate('/login')
-                }
-                console.log(data)
-             })()
-        }catch(err){
-         console.log(err)
+            if (data.status === 'success') {
+                navigate('/login');
+            }
+            console.log(data);
+        } catch (err) {
+            console.error(err);
         }
-    }
-  return(
-    <div style={{height:'100vh',width:'100vw',backgroundImage:'url(https://platinmods.com/attachments/sonyliv-jarvismods-png.240209/)',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
-      <div style={{display:"flex",justifyContent:'center',alignItems:'center',position:'relative',top:'100px'}}>
-<div class="w-full max-w-xs" style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)', padding: '20px', borderRadius: '10px' ,position:'relative',left:'400px'}}>
-  <form  onChange={handleInput} onSubmit={doPost}>
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input  class="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-    </div>
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-        Email
-      </label>
-      <input class="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email"/>
-    </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input class="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-      {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
-    </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-        Sign In
-      </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/login">
-        Update Password?
-      </a>
-    </div>
-  </form>
-  <p class="text-center text-gray-5000 text-md">
-    &copy;2020 Already have an account?<Link
-     to='/login'>login</Link>
-  </p>
-</div>
-</div>
-    </div>
+    };
 
-  )
+    return (
+        <div className="min-h-screen bg-cover" style={{ backgroundImage: 'url(https://platinmods.com/attachments/sonyliv-jarvismods-png.240209/)' }}>
+            <div className="flex justify-center items-center h-screen">
+                <div className="w-full max-w-md bg-white bg-opacity-30 p-6 rounded-lg">
+                    <form onChange={handleInput} onSubmit={doPost}>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Username
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                        </div>
+                        <div className="flex flex-col md:flex-row items-center justify-between">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-2 md:mb-0" type="submit">
+                                Sign In
+                            </button>
+                            <Link to="/login" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                                Update Password?
+                            </Link>
+                        </div>
+                    </form>
+                    <p className="text-center text-gray-500 text-md" style={{color:'white'}}>
+                        &copy; 2020 Already have an account? <Link to="/login"><span style={{color:'#F6FFA6',fontWeight:'bold',fontSize:'20px'}}>Login</span></Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/Components/component/resetPassword.js
+++ src/Components/component/resetPassword.js
@@ -36,36 +36,37 @@ const ResetPassword=()=>{
         }
     } 
     return(
-        <div style={{display:"flex",justifyContent:'center',alignItems:'center',marginTop:'100px'}}>
+      <div style={{height:'100vh',width:'100vw',backgroundRepeat:'no-repeat',backgroundSize:'cover', backgroundImage:`url(https://w0.peakpx.com/wallpaper/848/320/HD-wallpaper-hanuman-pc-cool-hanuman.jpg)`}}>
+         <div style={{display:"flex",justifyContent:'center',alignItems:'center',position:'relative',top:'100px'}}>
         <div class="w-full max-w-xs" >
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onChange={handleInput} onSubmit={doPost}>
+  <form class="bg-transparent shadow-md rounded px-8 pt-6 pb-8 mb-4" onChange={handleInput} onSubmit={doPost}>
   <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+      <label class="block text-blue-700 text-sm font-bold mb-2" for="name">
         Name
       </label>
-      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="name"/>
+      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent focus:shadow-outline"  id="name" type="text" placeholder="name"/>
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+      <label class="block text-blue-700 text-sm font-bold mb-2" for="email">
         Email
       </label>
-      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email"/>
+      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent focus:shadow-outline" id="email" type="email" placeholder="email"/>
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="passwordCurrent">
+      <label class="block text-blue-700 text-sm font-bold mb-2" for="passwordCurrent">
         PasswordCurrent
       </label>
-      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="passwordCurrent" type="password" placeholder="passwordcurrent"/>
+      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none bg-transparent focus:shadow-outline" id="passwordCurrent" type="password" placeholder="passwordcurrent"/>
     </div>
     <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+      <label class="block text-blue-700 text-sm font-bold mb-2" for="password">
         Password
       </label>
-      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+      <input class="shadow appearance-none border border-black rounded w-full py-2 px-3 text-white-700 mb-3 leading-tight focus:outline-none  bg-transparent focus:shadow-outline" id="password" type="password" placeholder="******************"/>
       {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
     </div>
     <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+      <button class="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Confirm
       </button>
       {/* <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
@@ -78,6 +79,8 @@ const ResetPassword=()=>{
   </p>
 </div>
 </div>
+      </div>
+       
     )
 }
 export default ResetPassword
